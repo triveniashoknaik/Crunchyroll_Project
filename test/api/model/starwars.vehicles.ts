b8350a6cea281c24172dc8f44bb2b19dd6b34aba
triveniashoknaik21 <triveniashoknaik21@gmail.com>
2024-03-09 17:45:43 -0600

    CrunchyRoll TakeHome Project

---
+++ test/api/model/starwars.vehicles.ts
@@ -0,0 +1,17 @@
+export class StarWarsVehicles
+{
+    private cargo_capacity: string = "50000";
+    private consumables: string = "2 months";
+    private cost_in_credits: string = "150000";
+    private crew: string = "46";
+    private length: string = "36.8 ";
+    private manufacturer: string = "Corellia Mining Corporation";
+    private max_atmosphering_speed: string = "30";
+    private model: string = "Digger Crawler";
+    private name: string = "Sand Crawler";
+    private passengers: string = "30";
+    private pilots: string[] = [];
+    private films: string[] = [];
+    private url: string =  "https://www.swapi.tech/api/vehicles/4";
+    private vehicle_class: string = "wheeled"
+}
